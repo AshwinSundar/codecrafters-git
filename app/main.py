c936cc34cd9657907c5bca664142f92054dfbe14
Ashwin Sundar <ashiundar@gmail.com>
2025-07-22 23:41:42 -0600

    adds git cat-file functionality

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 import sys
 import os
+import zlib
+
+
+def find_nth(string, substring, n):
+    '''Helper for extract_content'''
+    if (n == 1):
+        return string.find(substring)
+    else:
+        return string.find(substring, find_nth(string, substring, n - 1) + 1)
+
+
+def extract_content(obj: str):
+    return find_nth(obj, " ", 2) + 1
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
-
-    # Uncomment this block to pass the first stage
-
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
@@ -16,6 +24,19 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+
+    elif command == "cat-file":
+        flag1 = sys.argv[2]
+        hash = sys.argv[3]
+
+        if flag1 != "-p":
+            raise RuntimeError(f"Unrecognized flag:{flag1}\nValid options: -p")
+
+        with open(f".git/objects/{hash[0:2]}/{hash[2:]}", "rb") as file:
+            obj_raw = file.read()
+            obj_decompressed = zlib.decompress(obj_raw).decode()
+            print(obj_decompressed[extract_content(obj_decompressed):])
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
