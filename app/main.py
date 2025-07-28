f2de4ca7e0aa745200fe302cb0bce58830960616
Ashwin Sundar <ashiundar@gmail.com>
2025-07-28 15:07:13 -0600

    adds hash-object functionality

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import zlib
+from hashlib import sha1
 
 
 def extract_content(obj: bytes) -> str:
@@ -28,6 +29,33 @@ def main():
         with open(f".git/objects/{hash[0:2]}/{hash[2:]}", "rb") as file:
             obj = zlib.decompress(file.read())
             print(extract_content(obj), end='')
+
+    elif command == "hash-object":
+        flag1 = sys.argv[2]
+        fileName = sys.argv[3]
+
+        if flag1 != "-w":
+            raise RuntimeError(f"Unrecognized flag:{flag1}\nValid options: -w")
+
+        with open(fileName, "rb") as file:
+            contents = file.read()
+            header = f"blob {len(contents)}\x00".encode()  # define header format
+
+            # compute hash of object
+            hash = sha1(header + contents).hexdigest()
+            print(hash)
+
+            # split up hash
+            objectsDir = '.git/objects'
+            dirName = hash[:2]
+            fileName = hash[2:]
+
+            if not os.path.isdir(f'{objectsDir}/{dirName}'):
+                os.mkdir(f'{objectsDir}/{dirName}')
+
+            # write compressed data to file
+            with open(f'{objectsDir}/{dirName}/{fileName}', 'wb') as out_file:
+                out_file.write(zlib.compress(header + contents))
 
     else:
         raise RuntimeError(f"Unknown command #{command}")
